dc6e1e89d387b7269bf1c315fe8f5ca1fec88dcf
Pshypher <bshote@getcarbon.co>
2025-02-18 21:22:43 +0100

    Completes exercise one

--- app/src/main/java/com/techyourchance/coroutines/exercises/exercise1/Exercise1Fragment.kt
+++ app/src/main/java/com/techyourchance/coroutines/exercises/exercise1/Exercise1Fragment.kt
@@ -14,6 +14,9 @@ import com.techyourchance.coroutines.R
 import com.techyourchance.coroutines.common.BaseFragment
 import com.techyourchance.coroutines.common.ThreadInfoLogger
 import com.techyourchance.coroutines.home.ScreenReachableFromHome
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class Exercise1Fragment : BaseFragment() {
 
@@ -23,6 +26,8 @@ class Exercise1Fragment : BaseFragment() {
     private lateinit var btnGetReputation: Button
 
     private lateinit var getReputationEndpoint: GetReputationEndpoint
+
+    private val coroutineScope = CoroutineScope(Dispatchers.Main.immediate)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -46,15 +51,17 @@ class Exercise1Fragment : BaseFragment() {
         btnGetReputation = view.findViewById(R.id.btn_get_reputation)
         btnGetReputation.setOnClickListener {
             logThreadInfo("button callback")
-            btnGetReputation.isEnabled = false
-            getReputationForUser(edtUserId.text.toString())
-            btnGetReputation.isEnabled = true
+            coroutineScope.launch {
+                btnGetReputation.isEnabled = false
+                getReputationForUser(edtUserId.text.toString())
+                btnGetReputation.isEnabled = true
+            }
         }
 
         return view
     }
 
-    private fun getReputationForUser(userId: String) {
+    private suspend fun getReputationForUser(userId: String) {
         logThreadInfo("getReputationForUser()")
 
         val reputation = getReputationEndpoint.getReputation(userId)
--- app/src/main/java/com/techyourchance/coroutines/exercises/exercise1/GetReputationEndpoint.kt
+++ app/src/main/java/com/techyourchance/coroutines/exercises/exercise1/GetReputationEndpoint.kt
@@ -1,13 +1,17 @@
 package com.techyourchance.coroutines.exercises.exercise1
 
 import com.techyourchance.coroutines.common.ThreadInfoLogger
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import kotlin.random.Random
 
 class GetReputationEndpoint {
-    fun getReputation(userId: String): Int {
-        ThreadInfoLogger.logThreadInfo("GetReputationEndpoint#getReputation(): called")
-        Thread.sleep(3000)
-        ThreadInfoLogger.logThreadInfo("GetReputationEndpoint#getReputation(): return data")
-        return Random.nextInt(0, 100)
+    suspend fun getReputation(userId: String): Int {
+        return withContext(Dispatchers.IO) {
+            ThreadInfoLogger.logThreadInfo("GetReputationEndpoint#getReputation(): called")
+            Thread.sleep(3000)
+            ThreadInfoLogger.logThreadInfo("GetReputationEndpoint#getReputation(): return data")
+            Random.nextInt(0, 100)
+        }
     }
 }
